71922903b903a2bf7ee07496fa013db22d86b537
Prasadmukalla12 <prasadmukalla124@gmail.com>
2026-02-20 20:12:35 +0530

    Update videolibraryHome.tsx

--- src/componant/videolibraryHome.tsx
+++ src/componant/videolibraryHome.tsx
@@ -6,7 +6,7 @@ export function VideoLibraryHome(){
 
     return(
         <div className="container-fluid">
-            <div className="d-flex justify-content-center align-items-center" style={{height:"500px"}}>
+            <div className="d-flex justify-content-center align-items-center bg-info-subtle" style={{height:"500px"}}>
                 <Link to="/admin-login" className="btn btn-primary">Admin</Link>
                 <Link to="/user-login" className="btn btn-warning mx-3">User</Link>
             </div>
